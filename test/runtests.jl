c41ee4783b45c46130ee097b56d6c1189847b4e0
DanielVandH <danj.vandenheuvel@gmail.com>
2022-10-02 12:04:24 +1000

    Restructured test file

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,10 +6,24 @@ using DelaunayTriangulation
 using OffsetArrays
 const DT = DelaunayTriangulation
 
-@testset "Primitives" begin include("primitives.jl") end
-@testset "Collections" begin include("collections.jl") end 
-@testset "Struct definitions" begin include("struct_definitions.jl") end 
-@testset "Struct updates" begin include("struct_updates.jl") end
-@testset "Predicates" begin include("predicates.jl") end
-@testset "Triangulation" begin include("triangulate.jl") end
-@testset "Utility functions" begin include("utils.jl") end
+@testset "Primitives" begin
+    include("primitives.jl")
+end
+@testset "Collections" begin
+    include("collections.jl")
+end
+@testset "Struct definitions" begin
+    include("struct_definitions.jl")
+end
+@testset "Struct updates" begin
+    include("struct_updates.jl")
+end
+@testset "Predicates" begin
+    include("predicates.jl")
+end
+@testset "Triangulation" begin
+    include("triangulate.jl")
+end
+@testset "Utility functions" begin
+    include("utils.jl")
+end
