eede6f3c42d412b8fbdd486ddc2dcfbab609618e
DanielVandH <danj.vandenheuvel@gmail.com>
2022-10-15 13:55:44 +1000

    Jump-and-march algorithm

--- src/locate_triangle.jl
+++ src/locate_triangle.jl
@@ -1,3 +1,8 @@
+"""
+    locate_triangle(HG::HistoryGraph, pts, r::I, init=find_root(HG; method=:rng)) where {I}
+
+Finds the triangle `T` containing the query point `r` using the history graph `HG`.
+"""
 function locate_triangle(HG::HistoryGraph, pts, r::I, init=find_root(HG; method=:rng)) where {I}
     if out_deg(HG, init) == 0
         return init, isintriangle(init, pts, r)
@@ -7,4 +12,54 @@ function locate_triangle(HG::HistoryGraph, pts, r::I, init=find_root(HG; method=
         isin = isintriangle(T, pts, r)
         (isin == I(1) || isin == I(0)) && return locate_triangle(HG, pts, r, T)
     end
+end
+
+"""
+    jump_and_march(k::I, q, adj::Adjacent{I,E}, adj2v::Adjacent2Vertex{I,Es,E}, pts;
+        TriangleType::Type{V}=NTuple{3,Int64}) where {I,E,Es,V}
+
+Uses the jump and march algorithm to locate the triangle `T` in the triangulation that 
+contains the query point, starting at the vertex `k`.
+"""
+function jump_and_march(k::I, q, adj::Adjacent{I,E}, adj2v::Adjacent2Vertex{I,Es,E}, pts;
+    TriangleType::Type{V}=NTuple{3,Int64}) where {I,E,Es,V}
+    p = get_point(pts, k)
+    i, j = rand(get_edge(adj2v, k))
+    pᵢ = get_point(pts, i)
+    pⱼ = get_point(pts, j)
+    # Find the initial triangle to start in
+    if orient(p, q, pⱼ) == I(1)
+        while orient(p, q, pᵢ) == I(1)
+            j = i
+            pⱼ = pᵢ
+            i = get_edge(adj, i, k)
+            i == 0 && return jump_and_march(j, q, adj, adj2v, pts; TriangleType)
+            pᵢ = get_point(pts, i)
+        end
+    else
+        while orient(p, q, pⱼ) == I(-1)
+            i = j
+            pᵢ = pⱼ
+            j = get_edge(adj, k, j)
+            j == 0 && return jump_and_march(i, q, adj, adj2v, pts; TriangleType)
+            pⱼ = get_point(pts, j)
+        end
+    end
+    # Now do the straight line search 
+    i, j = j, i
+    pᵢ, pⱼ = pⱼ, pᵢ # pᵢ is left of pq, pⱼ is right of pq 
+    while orient(pᵢ, pⱼ, q) == I(1)
+        k = get_edge(adj, i, j)
+        pₖ = get_point(pts, k)
+        if orient(p, q, pₖ) == I(-1)
+            j = k
+            pⱼ = pₖ
+        else
+            i = k
+            pᵢ = pₖ
+        end
+    end
+    # Swap the orientation to get a positively oriented triangle 
+    k = get_edge(adj, j, i)
+    return construct_triangle(V, j, i, k)
 end
---
+++ test/point_location.jl
@@ -0,0 +1,40 @@
+@testset "Point location with DAG or jump-and-march" begin
+    for n = 3:350
+        @show n
+        r = 5sqrt.(rand(n))
+        θ = 2π * rand(n)
+        pts = [@SVector[r * cos(θ), r * sin(θ)] for (r, θ) in zip(r, θ)]
+        T, adj, adj2v, DG, HG = DT.triangulate_berg(pts)
+        _pts = deepcopy(pts)
+        for k in rand(1:n, ceil(Int64, 0.45n^(1 / 3)))
+            for _ in 1:length(T)
+                sample_T = rand(T)
+                a, b, c = DT.get_point(pts, sample_T...)
+                q = (a + b + c) / 3
+                push!(_pts, q)
+                τ, flag = locate_triangle(HG, _pts, n + 1, DT.BoundingTriangle)
+                @test DT.isintriangle(τ, _pts, n + 1) == flag == 1
+                @test DT.isoriented(τ, pts) == 1
+                for _ in 1:4
+                    τ = jump_and_march(k, q, adj, adj2v, pts)
+                    @test DT.isintriangle(τ, _pts, n + 1) == 1
+                    @test DT.isoriented(τ, pts) == 1
+                end
+                pop!(_pts)
+
+                for q in (a, b, c)
+                    push!(_pts, q)
+                    τ, flag = locate_triangle(HG, _pts, n + 1, DT.BoundingTriangle)
+                    @test DT.isintriangle(τ, _pts, n + 1) == flag == 0
+                    @test DT.isoriented(τ, pts) == 1
+                    for _ in 1:4
+                        τ = jump_and_march(k, q, adj, adj2v, pts)
+                        @test DT.isintriangle(τ, _pts, n + 1) == 0
+                        @test DT.isoriented(τ, pts) == 1
+                    end
+                    pop!(_pts)
+                end
+            end
+        end
+    end
+end
