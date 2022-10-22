4fadec71e0f1f9cd77d665de9dd4270cb665e781
DanielVandH <danj.vandenheuvel@gmail.com>
2022-10-22 12:54:22 +1000

    Put the selection of an initial triangle into its own function

--- src/locate_triangle.jl
+++ src/locate_triangle.jl
@@ -51,6 +51,44 @@ a triangulation. That is, the initial point will not be `q`.
 function select_initial_point(pts, q::Integer; pt_idx=eachindex(pts), m=ceil(Int64, length(pt_idx)^(1 / 3)))
     return select_initial_point(pts, get_point(pts, q); m, pt_idx)
 end
+
+"""
+    select_initial_triangle(q, adj::Adjacent{I, E}, adj2v, k, pts) where {I, E}
+
+Given a query point `q` and an initial point `k` (from e.g. [`select_initial_point`](@ref)),
+finds the triangle `(i, j, k)` to start the jump and march algorithm (see [`jump_and_march`](@ref)) in.
+Returns `p, i, j, pᵢ, pⱼ` such that `pᵢ = get_point(pts, i)`, `pⱼ = get_point(pts, j)`, 
+`p = get_point(pts, k)`, and `pᵢ` is to the left of `pq` and `pⱼ` is to the right of `pq`.
+"""
+function select_initial_triangle(q, adj::Adjacent{I, E}, adj2v, k, pts) where {I, E}
+    p = get_point(pts, k)
+    i, j = rand(get_edge(adj2v, k))
+    pᵢ = get_point(pts, i)
+    pⱼ = get_point(pts, j)
+    # Find the initial triangle to start in
+    if orient(p, q, pⱼ) == I(1)
+        while orient(p, q, pᵢ) == I(1)
+            j = i
+            pⱼ = pᵢ
+            i = get_edge(adj, i, k)
+            i == I(BoundaryIndex) && return select_initial_triangle(q, adj, adj2v, j, pts)
+            pᵢ = get_point(pts, i)
+        end
+    else
+        while orient(p, q, pⱼ) == I(-1)
+            i = j
+            pᵢ = pⱼ
+            j = get_edge(adj, k, j)
+            j == I(BoundaryIndex) && return select_initial_triangle(q, adj, adj2v, i, pts)
+            pⱼ = get_point(pts, j)
+        end
+    end
+    # Now do the straight line search 
+    i, j = j, i
+    pᵢ, pⱼ = pⱼ, pᵢ # pᵢ is left of pq, pⱼ is right of pq 
+    return p, i, j, pᵢ, pⱼ
+end
+
 """
     jump_and_march(q, adj::Adjacent{I,E}, adj2v::Adjacent2Vertex{I,Es,E}, pts;
         pt_idx = eachindex(pts),
@@ -64,31 +102,7 @@ function jump_and_march(q, adj::Adjacent{I,E}, adj2v::Adjacent2Vertex{I,Es,E}, p
     pt_idx=eachindex(pts), m=ceil(Int64, length(pt_idx)^(1 / 3)),
     k=select_initial_point(pts, q; m, pt_idx),
     TriangleType::Type{V}=NTuple{3,Int64}) where {I,E,Es,V}
-    p = get_point(pts, k)
-    i, j = rand(get_edge(adj2v, k))
-    pᵢ = get_point(pts, i)
-    pⱼ = get_point(pts, j)
-    # Find the initial triangle to start in
-    if orient(p, q, pⱼ) == I(1)
-        while orient(p, q, pᵢ) == I(1)
-            j = i
-            pⱼ = pᵢ
-            i = get_edge(adj, i, k)
-            i == I(BoundaryIndex) && return jump_and_march(q, adj, adj2v, pts; TriangleType, k=j)
-            pᵢ = get_point(pts, i)
-        end
-    else
-        while orient(p, q, pⱼ) == I(-1)
-            i = j
-            pᵢ = pⱼ
-            j = get_edge(adj, k, j)
-            j == I(BoundaryIndex) && return jump_and_march(q, adj, adj2v, pts; TriangleType, k=i)
-            pⱼ = get_point(pts, j)
-        end
-    end
-    # Now do the straight line search 
-    i, j = j, i
-    pᵢ, pⱼ = pⱼ, pᵢ # pᵢ is left of pq, pⱼ is right of pq 
+    p, i, j, pᵢ, pⱼ = select_initial_triangle(q, adj, adj2v, k, pts)
     while orient(pᵢ, pⱼ, q) == I(1)
         k = get_edge(adj, i, j)
         k == I(BoundaryIndex) && return jump_and_march(q, adj, adj2v, pts; TriangleType, k=i)
