0961e74c0cd5e0ca657efc8ad2f66d86895a00eb
DanielVandH <danj.vandenheuvel@gmail.com>
2022-10-25 01:21:57 +1000

    Fix jump and march arguments

--- src/bowyerwatson.jl
+++ src/bowyerwatson.jl
@@ -2,7 +2,7 @@ function add_point_bowyer!(T::Ts, adj, adj2v, DG, pts, r;
     pt_idx=graph(DG).V, m=ceil(Int64, length(pt_idx)^(1 / 3)),
     initial_search_point=select_initial_point(pts, r; pt_idx, m)) where {Ts} # only works for inside points currently. also fails for collinear points
     tri_type = triangle_type(Ts)
-    V = jump_and_march(r, adj, adj2v, pts; k=initial_search_point, TriangleType=tri_type)
+    V = jump_and_march(r, adj, adj2v, DG, pts; k=initial_search_point, TriangleType=tri_type)
     i, j, k = indices(V)
     delete_triangle!(i, j, k, T, adj, adj2v, DG; protect_boundary=true)
     dig_cavity!(r, i, j, T, adj, adj2v, DG, pts)
