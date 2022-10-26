70486e87c5134abf5414d18211b659b1dc26d389
DanielVandH <danj.vandenheuvel@gmail.com>
2022-10-26 19:02:30 +1000

    More bowyer-watson

--- src/bowyerwatson.jl
+++ src/bowyerwatson.jl
@@ -16,7 +16,7 @@ function dig_cavity!(r, i, j, T, adj::Adjacent{I,E}, adj2v, DG, pts) where {I,E}
     if ℓ == I(DefaultAdjacentValue)
         return nothing # The triangle has already been deleted in this case 
     end
-    δ = ℓ ≠ I(BoundaryIndex) && isincircle(pts, r, i, j, ℓ)# Boundary edges form part of the boundary on the polygonal cavity if we have reached ℓ = BoundaryIndex
+    δ = ℓ ≠ I(BoundaryIndex) && isincircle(pts, r, i, j, ℓ)# Boundary edges form part of the boundary on the polygonal cavity if we have reached ℓ = BoundaryIndex. This will not cause issues with the ghost triangles since, if these ghost triangles are relevant, we will have started in one (since this means the point must be in the exterior), and so one of the dig cavity calls will have found it.  
     if δ == I(1)
         delete_triangle!(j, i, ℓ, T, adj, adj2v, DG; protect_boundary=true, update_ghost_edges=false) # Notice that we don't need the update_ghost_edges argument, since we will step over ghost edges already and add them into the triangulation if necessary
         dig_cavity!(r, i, ℓ, T, adj, adj2v, DG, pts)
@@ -27,6 +27,9 @@ function dig_cavity!(r, i, j, T, adj::Adjacent{I,E}, adj2v, DG, pts) where {I,E}
     return nothing
 end
 
+"""
+
+"""
 function triangulate_bowyer(pts;
     IntegerType::Type{I}=Int64,
     EdgeType::Type{E}=NTuple{2,IntegerType},
@@ -41,10 +44,12 @@ function triangulate_bowyer(pts;
     adj = Adjacent{I,E}()
     adj2v = Adjacent2Vertex{I,Es,E}()
     DG = DelaunayGraph{I}()
-    initial_triangle = construct_positively_oriented_triangle(TriangleType,
-        I(pt_order[begin]), I(pt_order[begin+1]), I(pt_order[begin+2]),
-        pts
-    )
+    initial_triangle = construct_positively_oriented_triangle(TriangleType, I(pt_order[begin]), I(pt_order[begin+1]), I(pt_order[begin+2]), pts)
+    while isoriented(initial_triangle, pts) == 0 # We cannot start with a degenerate triangle
+        pt_order = collect(pt_order) # We need to collect so that we can change the order of the points 
+        reverse!(@view pt_order[begin:(begin+2)]) # This makes it counter-clockwise
+        initial_triangle = construct_positively_oriented_triangle(TriangleType, I(pt_order[begin]), I(pt_order[begin+1]), I(pt_order[begin+2]), pts)
+    end
     u, v, w = indices(initial_triangle)
     add_triangle!(u, v, w, T, adj, adj2v, DG; update_ghost_edges=true)
     compute_centroid!(@view pts[pt_order[begin:(begin+2)]])
@@ -53,7 +58,7 @@ function triangulate_bowyer(pts;
         last_inserted_point = pt_order[last_inserted_point_number]
         pt_idx = @view pt_order[begin:last_inserted_point_number] # + 3 for the first three points already inserted
         m = ceil(Int64, length(pt_idx)^(1 / 3))
-        initial_search_point = select_initial_point(pts, new_point; pt_idx, m, try_points=try_last_inserted_point ? last_inserted_point : ())
+        initial_search_point = I(select_initial_point(pts, new_point; pt_idx, m, try_points=try_last_inserted_point ? last_inserted_point : ()))
         add_point_bowyer!(T, adj, adj2v, DG, pts, new_point;
             pt_idx, m, initial_search_point)
         update_centroid_after_new_point!(pts, new_point)
--- test/bowyerwatson.jl
+++ test/bowyerwatson.jl
@@ -127,3 +127,64 @@ end
       @test !DT.compare_deberg_to_bowyerwatson(T, adj, adj2v, DG, _T, _adj, _adj2v, _DG)
       @test !DT.compare_deberg_to_bowyerwatson(T, adj, adj2v, DG, pts)
 end
+
+@testset "A small example" begin
+      Random.seed!(929266666)
+      p1 = @SVector[-3.32, 3.53]
+      p2 = @SVector[-5.98, 2.17]
+      p3 = @SVector[-6.36, -1.55]
+      p4 = @SVector[-2.26, -4.31]
+      p5 = @SVector[6.34, -3.23]
+      p6 = @SVector[-3.24, 1.01]
+      p7 = @SVector[0.14, -1.51]
+      p8 = @SVector[0.2, 1.25]
+      p9 = @SVector[1.0, 4.0]
+      p10 = @SVector[4.74, 2.21]
+      p11 = @SVector[2.32, -0.27]
+      p12 = @SVector[4.382, 3.2599]
+      p13 = @SVector[-5.25303, 4.761]
+      p14 = @SVector[-9.83801, 0.562]
+      p15 = @SVector[-7.15986, -5.99]
+      p16 = @SVector[4.79, 2.74]
+      p17 = @SVector[3.77, 2.7689]
+      p18 = @SVector[15.19, 15.40]
+      p19 = @SVector[22.86, 9.93]
+      p20 = @SVector[25.96, -4.457]
+      p21 = @SVector[20.298, -13.444]
+      p22 = @SVector[4.626, -14.2757]
+      p23 = @SVector[-4.739, -14.313]
+      p24 = @SVector[-7.3448, -14.50]
+      p25 = @SVector[-10.06, -14.35]
+      p26 = @SVector[-12.97, -13.63]
+      p27 = @SVector[-15.46, -11.405]
+      p28 = @SVector[-16.899, -4.419]
+      p29 = @SVector[-15.766, 5.4368]
+      p30 = @SVector[-11.31, 11.367]
+      p31 = @SVector[-2.66, 14.95]
+      p32 = @SVector[6.58, 15.746]
+      pts = [p1, p2, p3, p4, p5, p6, p7, p8,
+            p9, p10, p11, p12, p13, p14, p15, p16, p17,
+            p18, p19, p20, p21, p22, p23, p24, p25,
+            p26, p27, p28, p29, p30, p31, p32]
+      for _ in 1:500
+            T, adj, adj2v, DG = DT.triangulate_bowyer(pts; trim=false)
+            _T, _adj, _adj2v, _DG, _HG = DT.triangulate_berg(pts)
+            @test DT.compare_deberg_to_bowyerwatson(T, adj, adj2v, DG, _T, _adj, _adj2v, _DG)
+      end
+end
+
+@testset "Larger random examples" begin
+      for r in 1:137
+            for IntegerType in (Int64, Int32, Int16)
+                  n = rand(3:1000)
+                  pts = rand(SVector{2,Float64}, n)
+                  T, adj, adj2v, DG = DT.triangulate_bowyer(pts; trim=false, IntegerType)
+                  @test DT.validate_triangulation(T, adj, adj2v, DG, pts)
+                  _T, _adj, _adj2v, _DG = deepcopy(T), deepcopy(adj), deepcopy(adj2v), deepcopy(DG)
+                  DT.remove_ghost_triangles!(_T, _adj, _adj2v, _DG)
+                  @test DT.validate_triangulation(_T, _adj, _adj2v, _DG, pts)
+                  DT.add_ghost_triangles!(_T, _adj, _adj2v, _DG)
+                  DT.compare_unconstrained_triangulations(T, adj, adj2v, DG, _T, _adj, _adj2v, _DG)
+            end
+      end
+end
