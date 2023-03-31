b05e2268dc222cd2c7e25f32a6e7ed5743ad19ed
DanielVandH <danj.vandenheuvel@gmail.com>
2023-04-01 08:43:58 +1000

    Fix a test

--- test/predicates/index_and_ghost_handling.jl
+++ test/predicates/index_and_ghost_handling.jl
@@ -3,11 +3,16 @@ const DT = DelaunayTriangulation
 using DataStructures
 using CairoMakie
 using StatsBase
+using ElasticArrays
 
 include("../helper_functions.jl")
 
 global x, y = complicated_geometry()
-global tri = generate_mesh(x, y, 2.0; convert_result=true, add_ghost_triangles=true)
+_tri = generate_mesh(x, y, 2.0; convert_result=true, add_ghost_triangles=true)
+_pts = ElasticMatrix(get_points(_tri))
+global tri = Triangulation(_pts, _tri.triangles, _tri.adjacent, _tri.adjacent2vertex, _tri.graph,
+    _tri.boundary_nodes, _tri.boundary_map, _tri.boundary_index_ranges,
+    _tri.constrained_edges, ConvexHull(_pts, _tri.convex_hull.indices))
 DT.compute_representative_points!(tri)
 global boundary_map = DT.get_boundary_map(tri)
 global pts = DT.get_points(tri)
@@ -78,14 +83,14 @@ end
                 @inferred DT.point_position_relative_to_circumcircle(tri, i, pts[:, j], k, ℓ)
             end
             q = (pts[:, i] .+ pts[:, j] .+ pts[:, k]) ./ 3
-            push!(pts, q)
+            append!(pts, q)
             ℓ = size(pts, 2)
             cert1 = DT.point_position_relative_to_circumcircle(i, j, k, ℓ, pts, boundary_map)
             cert2 = DT.point_position_relative_to_circumcircle(T, ℓ, pts, boundary_map)
             cert3 = DT.point_position_relative_to_circumcircle(tri, T, ℓ)
             cert4 = DT.point_position_relative_to_circumcircle(tri, i, j, k, ℓ)
             @test all(DT.is_inside, (cert1, cert2, cert3, cert4))
-            resize!(pts, ℓ - 1)
+            resize!(pts, 2, ℓ - 1)
         end
     end
 
