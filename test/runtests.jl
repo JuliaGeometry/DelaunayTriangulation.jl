f1224bf0596174d132787067c3975326ade26d2e
DanielVandH <danj.vandenheuvel@gmail.com>
2023-04-17 18:34:09 +1000

    forgot to uncomment tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,13 +2,6 @@ using DelaunayTriangulation
 using Test
 using SafeTestsets
 
-
-@safetestset "Documentation images" begin
-    include("doc_images.jl")
-end
-
-
-#=
 @testset verbose = true "DelaunayTriangulation" begin
     @testset verbose = true "Triangulation" begin
         @safetestset "Gmsh" begin
@@ -185,5 +178,3 @@ end
         end
     end
 end
-
-=#
