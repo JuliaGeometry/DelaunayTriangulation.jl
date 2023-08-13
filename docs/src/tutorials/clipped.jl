5f2c0c99c6edfbeb8c8b2d49eb9e00e4e0ae31f3
DanielVandH <danj.vandenheuvel@gmail.com>
2023-08-13 17:23:01 +1000

    Clipped and centroidal

--- docs/src/tutorials/clipped.jl
+++ docs/src/tutorials/clipped.jl
@@ -8,4 +8,32 @@
 # are on the to-do list, but they are not yet implemented - see [Issue #48](https://github.com/DanielVandH/DelaunayTriangulation.jl/issues/48).)
 
 # In the example below, we clip the tessellation to the convex hull of the point set by setting 
-# the second argument of `voronoi` to `true`.
+# the second argument of `voronoi` to `true`. 
+using DelaunayTriangulation
+using CairoMakie
+using StableRNGs
+using ReferenceTests #src 
+using Test #src
+fig_path = joinpath(@__DIR__, "../figures") #src
+
+rng = StableRNG(123)
+points = randn(rng, 2, 50)
+tri = triangulate(points; rng)
+vorn = voronoi(tri)
+
+#-
+clipped_vorn = voronoi(tri, true)
+
+# Note that the clipping has put more polygon vertices in. We compare 
+# the clipped tessellations below. 
+fig = Figure()
+ax1 = Axis(fig[1, 1], title="Unclipped", width=600, height=400)
+ax2 = Axis(fig[1, 2], title="Clipped", width=600, height=400)
+voronoiplot!(ax1, vorn, show_generators=false, colormap=:matter, strokewidth=4)
+voronoiplot!(ax2, clipped_vorn, show_generators=false, colormap=:matter, strokewidth=4)
+resize_to_layout!(fig)
+fig
+@test_reference joinpath(fig_path, "voronoi_ex_2.png") fig #src
+
+# As you can see, the unbounded polygons, and any polygons that included points 
+# outside of the convex hull, have now been clipped to the convex hull.
