1a3294ebd527fc0fab26cc38b6c374c74f7b8e85
DanielVandH <danj.vandenheuvel@gmail.com>
2023-08-15 21:47:52 +1000

    simplify

--- docs/liveserver.jl
+++ docs/liveserver.jl
@@ -1,18 +1,9 @@
-const repo_root = dirname(@__DIR__)
 import Pkg
 Pkg.activate(@__DIR__)
 Pkg.instantiate()
 import LiveServer
 withenv("LIVESERVER_ACTIVE" => "true") do
     LiveServer.servedocs(;
-        # Documentation root where make.jl and src/ are located
-        foldername=joinpath(repo_root, "docs"),
-        # Extra source folder to watch for changes
-        include_dirs=[
-            # Watch the src folder so docstrings can be Revise'd
-            joinpath(repo_root, "src"),
-        ],
-        doc_env=false,
         launch_browser=true
     )
 end
