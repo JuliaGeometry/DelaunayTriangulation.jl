8606710642046158e955d83dcaa02fe2f6fca9c1
Daniel VandenHeuvel <95613936+DanielVandH@users.noreply.github.com>
2024-10-01 14:44:02 +0100

    Use precompiletools.jl (#200)

--- src/DelaunayTriangulation.jl
+++ src/DelaunayTriangulation.jl
@@ -6,6 +6,7 @@ import ExactPredicates as EP
 import AdaptivePredicates as AP
 import EnumX
 import Random
+import PrecompileTools
 
 abstract type AbstractPredicateKernel end # needs to be defined early for use in data_structures.jl
 const PredicateCacheType = Union{Nothing, <:Tuple}
@@ -18,5 +19,7 @@ include("algorithms.jl")
 include("validation.jl")
 include("exports.jl")
 include("public.jl")
+
+include("precompile.jl")
 
 end
