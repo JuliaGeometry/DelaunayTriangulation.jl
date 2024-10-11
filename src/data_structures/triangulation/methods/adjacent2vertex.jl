beb04b098e56352ce87efc67b71b995a0bba3a34
Daniel VandenHeuvel <95613936+DanielVandH@users.noreply.github.com>
2024-10-11 23:41:37 +0100

    Adjacent2Vertex

--- src/data_structures/triangulation/methods/adjacent2vertex.jl
+++ src/data_structures/triangulation/methods/adjacent2vertex.jl
@@ -4,3 +4,5 @@
 Returns the set of all edges `(u, v)` in `tri` such that `(u, v, w)` is a positively oriented triangle in `tri`.
 """
 get_adjacent2vertex(tri::Triangulation, w) = get_adjacent2vertex(get_adjacent2vertex(tri), w)
+
+concretize_adjacent2vertex(tri::Triangulation) = concretize_adjacent2vertex(get_adjacent2vertex(tri))
