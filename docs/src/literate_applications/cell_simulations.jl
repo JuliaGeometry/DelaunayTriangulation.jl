dc82934cb3a029056ba0a189b294704134934229
Daniel VandenHeuvel <95613936+DanielVandH@users.noreply.github.com>
2024-10-30 17:33:49 +0000

    Update cell_simulations.jl

--- docs/src/literate_applications/cell_simulations.jl
+++ docs/src/literate_applications/cell_simulations.jl
@@ -1,3 +1,3 @@
 # # Cellular Biology 
 # This example has been moved to Agent.jl's documentation. Please 
-# see the example [here]().
+# see the example [here](https://juliadynamics.github.io/Agents.jl/stable/examples/delaunay/).
