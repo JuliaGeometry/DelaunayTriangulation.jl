776255d375ceda37eb967f5bb218192ac1afe6c8
Documenter.jl <documenter@juliadocs.github.io>
2024-11-24 00:29:54 +0000

    delete history

---
+++ previews/PR160/literate_tutorials/convex.jl
@@ -0,0 +1,59 @@
+# # Triangulating Convex Polygons 
+# 
+
+# In this tutorial, we show how we can triangulate convex 
+# polygons. The function [`triangulate_convex`](@ref) is used for this.
+# Let us start with a simple example.
+using DelaunayTriangulation
+using CairoMakie
+using ReferenceTests #src
+using Test #src
+using Preferences #src
+fig_path = joinpath(@__DIR__, "../figures") #src
+
+points = [
+    (10.0, 12.0), (7.0, 11.0), (8.0, 6.0),
+    (10.0, 3.0), (14.0, 5.0), (15.0, 10.0),
+    (13.0, 12.0)
+]
+S = 1:7
+tri = triangulate_convex(points, 1:7)
+
+#-
+fig, ax, sc = triplot(tri)
+fig 
+!USE_INEXACTPREDICATES && @test_reference joinpath(fig_path, "convex_ex_1.png") fig #src
+
+# This `tri` is our triangulation of the convex polygon. 
+# The first input is the set of points, and `S` defines 
+# the vertices to take from these points and their order, 
+# which must be provided in a counter-clockwise order.
+# Note that points does not have to contain only the points 
+# of the polygon, since `S` will be used to define the points 
+# needed.
+
+# Let us give a larger example. For simplicity, we triangulate a  
+# a discretised circle. 
+θ = LinRange(0, 2π, 5000) |> collect 
+pop!(θ)
+x = cos.(θ)
+y = sin.(θ)
+points = tuple.(x, y)
+S = 1:4999 # can also be [1:4999; 1], if you want the array to be circular 
+tri = triangulate_convex(points, S)
+
+#-
+fig, ax, sc = triplot(tri)
+fig 
+!USE_INEXACTPREDICATES && @test_reference joinpath(fig_path, "convex_ex_2.png") fig #src
+
+# Here is a comparison of the time it takes to triangulate this 
+# using `triangulate_convex` or `triangulate`.
+using BenchmarkTools 
+@benchmark triangulate_convex($points, $S) 
+
+#-
+@benchmark triangulate($points) 
+
+# For the smaller example that we started with above, `triangulate_convex` is also 
+# faster, although not by much (≈15.10 μs versus ≈10.7 μs).
---
+++ previews/PR160/literate_tutorials/lattice.jl
@@ -0,0 +1,54 @@
+# # Triangulating Rectangular Regions 
+# 
+
+# In this tutorial, we show how you can easily 
+# triangulate rectangular regions of the form 
+# $[a, b] \times [c, d]$. Rather than using `triangulate`,
+# you can use [`triangulate_rectangle`](@ref) for this purpose. To start, 
+# we give a simple example 
+using DelaunayTriangulation
+using CairoMakie
+using ReferenceTests #src
+using Test #src
+fig_path = joinpath(@__DIR__, "../figures") #src
+
+a, b, c, d = 0.0, 2.0, 0.0, 10.0
+nx, ny = 10, 25
+tri = triangulate_rectangle(a, b, c, d, nx, ny)
+fig, ax, sc = triplot(tri)
+fig
+@test_reference joinpath(fig_path, "rectangle_ex_1.png") fig #src
+
+# This can be much faster than if we just construct the points in the 
+# lattice manually and `triangulate` those. Here's a comparison of the times. 
+using BenchmarkTools
+points = get_points(tri)
+@benchmark triangulate($points; randomise=$false) # randomise=false because points are already in lattice order, i.e. spatially sorted
+
+#-
+@benchmark triangulate_rectangle($a, $b, $c, $d, $nx, $ny)
+
+# This difference would be more pronounced for larger `nx, ny`.
+
+# Note that the output of `triangulate_rectangle` treats the boundary 
+# as a constrained boundary: 
+get_boundary_nodes(tri)
+@test !isempty(get_boundary_nodes(tri)) #src
+@test DelaunayTriangulation.has_boundary_nodes(tri) #src
+
+# This boundary is split into four separate sections, one for each 
+# side of the rectangle. If you would prefer to keep the boundary as one 
+# contiguous section, use `single_boundary=true`. Moreover, note that 
+# this `tri` has ghost triangles:
+tri
+@test DelaunayTriangulation.has_ghost_triangles(tri) #src
+
+# You can opt into not having these by using `delete_ghosts=true`:
+tri = triangulate_rectangle(a, b, c, d, nx, ny; single_boundary=true, delete_ghosts=true)
+tri
+
+#-
+get_boundary_nodes(tri)
+
+#-
+DelaunayTriangulation.has_ghost_triangles(tri)
